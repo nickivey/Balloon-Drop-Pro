8dbcafa0f1888e425c74e10e18e31e52b6898e97
Nick Ivey <nickivey@Nicks-MacBook-Pro.local>
2014-02-23 15:19:17 -0500

    Initial Commit

---
+++ Ballon Drop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ballon Drop
+//
+//  Created by Nick Ivey on 2/23/14.
+//  Copyright (c) 2014 Nick Ivey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ballon DropTests/Ballon_DropTests.m
@@ -0,0 +1,34 @@
+//
+//  Ballon_DropTests.m
+//  Ballon DropTests
+//
+//  Created by Nick Ivey on 2/23/14.
+//  Copyright (c) 2014 Nick Ivey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ballon_DropTests : XCTestCase
+
+@end
+
+@implementation Ballon_DropTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
